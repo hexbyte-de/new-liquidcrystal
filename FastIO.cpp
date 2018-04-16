2cde664190dfb4f6f22abf37103685f9f16bb39f
fmalpartida <fmalpartida@gmail.com>
2018-04-15 20:53:28 -0700

    Minor updates to the license to cater for future GPL versions.

--- FastIO.cpp
+++ FastIO.cpp
@@ -4,7 +4,8 @@
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License.
+//    the Free Software Foundation, either version 3 of the License, or
+//    any later version.
 //
 //    This program is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,6 +17,7 @@
 //    If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
 // 
 // ---------------------------------------------------------------------------
+//
 // fio_shiftOut1 functions are based on Shif1 protocol developed by Roman Black 
 // (http://www.romanblack.com/shift1.htm)
 //
